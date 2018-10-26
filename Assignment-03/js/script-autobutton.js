5b1d6d402f48130b01a4dd80bc6c5c2cfe050646
jtkfrancisco <43385696+jtkfrancisco@users.noreply.github.com>
2018-10-26 17:58:07 -0400

    Auto Movement Update
    Channel buttons now make the opacity change automatically rather than toggle the channel on or off.

--- Assignment-03/js/script-autobutton.js
+++ Assignment-03/js/script-autobutton.js
@@ -9,9 +9,7 @@ var verticalCener = Math.floor(window.innerHeight/2);
 var r = 745;
 var xcenter = horizontalCenter; // center X position
 var ycenter = 0 // center Y position
-var t = 0;
-var t2 = 0;
-var t3 = 0; //time
+var t = 0; //time
 var divisor=745*2;
 
 
@@ -21,15 +19,15 @@ function moveitRed(){
     //var newTop = Math.floor(ycenter + (r * Math.sin(t)));
     var newTop = ycenter;
 
-    $('.testDiv2').animate({top: newTop,left: newLeft,}, 1, function(){moveitRed()});
+    $('.testDiv').animate({top: newTop,left: newLeft,}, 1, function(){moveitRed()});
 
     $(".redChannel").css("opacity",newLeft/divisor)
     $(".redButton").text(newLeft/divisor)
 }
 
 function moveitGreen(){
-    t2 += 0.01;
-    var newLeft = Math.floor(xcenter + (r * Math.cos(t2)));
+    t += 0.01;
+    var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
     //var newTop = Math.floor(ycenter + (r * Math.sin(t)));
     var newTop = ycenter;
 
@@ -40,38 +38,38 @@ function moveitGreen(){
 }
 
 function moveitBlue(){
-    t3 += 0.01;
-    var newLeft = Math.floor(xcenter + (r * Math.cos(t3)));
+    t += 0.01;
+    var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
     //var newTop = Math.floor(ycenter + (r * Math.sin(t)));
     var newTop = ycenter;
 
-    $('.testDiv3').animate({top: newTop,left: newLeft,}, 1, function(){moveitBlue()});
+    $('.testDiv').animate({top: newTop,left: newLeft,}, 1, function(){moveitBlue()});
 
     $(".blueChannel").css("opacity",newLeft/divisor)
     $(".blueButton").text(newLeft/divisor)
 }
 
-// $(".autoButton").click(function(){
-// 	if (autoOn==false){
-// 		$(".autoButton").text("Auto-ON");
-// 		autoOn=true;
-// 		moveitRed();
-// 		moveitGreen();
-// 		moveitBlue();
-// 	} else {
-// 		$(".autoButton").text("Auto-OFF");
-// 		autoOn=false;
-// 	}
-// })
+
+$(".autoButton").click(function(){
+	if (autoOn==false){
+		$(".autoButton").text("Auto-ON");
+		autoOn=true;
+		moveitRed();
+		moveitGreen();
+		moveitBlue();
+	} else {
+		$(".autoButton").text("Auto-OFF");
+		autoOn=false;
+	}
+})
 
 $(".redButton").click(function(){
 	if (redOn==true){
-		// $(".redChannel").css("opacity","0"); //old functionality to turn off colour channel
+		$(".redChannel").css("opacity","0");
 		$(".redButton").text("OFF")
-		moveitRed();
 		redOn=false;
 	} else {
-		// $(".redChannel").css("opacity","1");
+		$(".redChannel").css("opacity","1");
 		$(".redButton").text("ON");
 		redOn=true;
 	}
@@ -79,41 +77,39 @@ $(".redButton").click(function(){
 
 $(".greenButton").click(function(){
 	if (greenOn==true){
-		// $(".greenChannel").css("opacity","0"); //old functionality to turn off colour channel
+		$(".greenChannel").css("opacity","0");
 		$(".greenButton").text("OFF");
-		moveitGreen();
 		greenOn=false;
 	} else {
-		// $(".greenChannel").css("opacity","1");
+		$(".greenChannel").css("opacity","1");
 		$(".greenButton").text("ON");
 		greenOn=true;
 	}
 })
 
-$(".blueButton").click(function(){ 
+$(".blueButton").click(function(){
 	if (blueOn==true){
-		// $(".blueChannel").css("opacity","0");//old functionality to turn off colour channel
-		$(".blueButton").text("OFF");
-		moveitBlue();
+		$(".blueChannel").css("opacity","0");
+		$(".blueButton").text("OFF")
 		blueOn=false;
 	} else {
-		// $(".blueChannel").css("opacity","1");
-		$(".blueButton").text("ON");
+		$(".blueChannel").css("opacity","1");
+		$(".blueButton").text("ON")
 		blueOn=true;
 	}
 })
 
-$("html").mousemove(function(){ //changes red and blue channel opacity based on mouse pos.
+$("html").mousemove(function(){
 	$(".blackSquare").text(event.pageX + ", " + event.pageY);
 
 	xPos=event.pageX/window.innerWidth;
 	$(".redChannel").css("opacity",xPos)
-	// $(".redButton").text(xPos)
+	$(".redButton").text(xPos)
 
 	yPos=event.pageY/window.innerHeight;
 	Math.round(yPos * 100) / 100;
 	$(".blueChannel").css("opacity",yPos)
-	// $(".blueButton").text(yPos)
+	$(".blueButton").text(yPos)
 })
 
 
